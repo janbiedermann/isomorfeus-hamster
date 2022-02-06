e07edcd71bc77753c03ee12fc322bc2290bf57af
Jan Biedermann <jan@kursator.de>
2022-02-06 08:30:08 +0100

    clarify serialization mode and allow to specify class_cache usage

--- lib/isomorfeus/hamster/marshal.rb
+++ lib/isomorfeus/hamster/marshal.rb
@@ -2,22 +2,22 @@ module Isomorfeus
   module Hamster
     module Marshal
       class << self
-        def dump(db, key, obj)
-          db.env.transaction { db.put(key, ::Oj.dump(obj)) }
+        def dump(db, key, obj, class_cache: true)
+          db.env.transaction { db.put(key, ::Oj.dump(obj, mode: :object, class_cache: class_cache)) }
         end
 
-        def load(db, key)
+        def load(db, key, class_cache: true)
           obj_j = db.get(key)
           return nil unless obj_j
-          ::Oj.load(obj_j)
+          ::Oj.load(obj_j, mode: :object, class_cache: class_cache)
         end
 
-        def serialize(obj)
-          ::Oj.dump(obj)
+        def serialize(obj, class_cache: true)
+          ::Oj.dump(obj, mode: :object, class_cache: class_cache)
         end
 
-        def unserialize(obj_j)
-          ::Oj.load(obj_j)
+        def unserialize(obj_j, class_cache: true)
+          ::Oj.load(obj_j, mode: :object, class_cache: class_cache)
         end
       end
     end
