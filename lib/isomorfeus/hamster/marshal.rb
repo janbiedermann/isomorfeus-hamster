91a4ec57b75a3291556bf784e4ebec03e34d7d52
Jan Biedermann <jan@kursator.de>
2022-02-18 06:58:30 +0100

    allow for circular objects

--- lib/isomorfeus/hamster/marshal.rb
+++ lib/isomorfeus/hamster/marshal.rb
@@ -3,21 +3,21 @@ module Isomorfeus
     module Marshal
       class << self
         def dump(db, key, obj, class_cache: true)
-          db.env.transaction { db.put(key, ::Oj.dump(obj, mode: :object, class_cache: class_cache)) }
+          db.env.transaction { db.put(key, ::Oj.dump(obj, mode: :object, circular: true, class_cache: class_cache)) }
         end
 
         def load(db, key, class_cache: true)
           obj_j = db.get(key)
           return nil unless obj_j
-          ::Oj.load(obj_j, mode: :object, class_cache: class_cache)
+          ::Oj.load(obj_j, mode: :object, circular: true, class_cache: class_cache)
         end
 
         def serialize(obj, class_cache: true)
-          ::Oj.dump(obj, mode: :object, class_cache: class_cache)
+          ::Oj.dump(obj, mode: :object, circular: true, class_cache: class_cache)
         end
 
         def unserialize(obj_j, class_cache: true)
-          ::Oj.load(obj_j, mode: :object, class_cache: class_cache)
+          ::Oj.load(obj_j, mode: :object, circular: true, class_cache: class_cache)
         end
       end
     end
