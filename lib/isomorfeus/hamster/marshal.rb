13e44bf93ca8c4915bdfb964e00984a40701f14b
Jan Biedermann <jan@kursator.de>
2022-02-18 07:56:03 +0100

    support compression

--- lib/isomorfeus/hamster/marshal.rb
+++ lib/isomorfeus/hamster/marshal.rb
@@ -2,14 +2,19 @@ module Isomorfeus
   module Hamster
     module Marshal
       class << self
-        def dump(db, key, obj, class_cache: true)
-          db.env.transaction { db.put(key, ::Oj.dump(obj, mode: :object, circular: true, class_cache: class_cache)) }
+        BROTLI = 'b:'.freeze
+
+        def dump(db, key, obj, class_cache: true, compress: false)
+          v = ::Oj.dump(obj, mode: :object, circular: true, class_cache: class_cache)
+          v = 'b:' << ::Brotli.deflate(v, quality: compress) if compress
+          db.env.transaction { db.put(key, v) }
         end
 
         def load(db, key, class_cache: true)
-          obj_j = db.get(key)
-          return nil unless obj_j
-          ::Oj.load(obj_j, mode: :object, circular: true, class_cache: class_cache)
+          v = db.get(key)
+          return nil unless v
+          v = ::Brotli.inflate(v[2..]) if v.start_with?(BROTLI)
+          ::Oj.load(v, mode: :object, circular: true, class_cache: class_cache)
         end
 
         def serialize(obj, class_cache: true)
